7089b6fcbbf7152645dad8e6579dac2226ec7955
Aaron Hu <haldenbiscuit@gmail.com>
2022-08-26 18:48:52 -0700

    Added Day 8

--- main.js
+++ main.js
@@ -57,4 +57,15 @@ function lovefunc(flower1, flower2){
     return Number((roll * 2) + position)
   }
   
-  
+
+//DAY 8: Wilson primes satisfy the following condition. Let P represent a prime number.
+// Then,((P-1)! + 1) / (P * P) should give a whole number. Your task is to create a function that returns true if the given number is a Wilson prime
+
+function amIWilson(P) {
+  const factorial = x => x <= 1 ? 1 : x * factorial(x - 1);
+ if((factorial(P - 1) + 1) / (P * P) % 1 === 0) {
+  return true
+ } else {
+  return false 
+ }
+}
