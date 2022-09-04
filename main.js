5001c0af8bbe89c53b3150260fb096a9b04c5fb8
Aaron Hu <haldenbiscuit@gmail.com>
2022-09-03 17:02:56 -0700

    Added Day 15

--- main.js
+++ main.js
@@ -121,3 +121,13 @@ function geometricSequenceElements(a, r, n){
   }
   return res.join(', ');
 }
+
+//Day 15: Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+// Examples:
+// 42, "number"   --> true
+// "42", "number" --> false
+
+function typeValidation(variable, type) {
+  return typeof variable === type; 
+}
