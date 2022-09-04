e49b11a06aeaae6e6e727de77b8865ce7a5a29ff
Aaron Hu <haldenbiscuit@gmail.com>
2022-09-03 22:33:56 -0700

    Added Day 16

--- main.js
+++ main.js
@@ -131,3 +131,13 @@ function geometricSequenceElements(a, r, n){
 function typeValidation(variable, type) {
   return typeof variable === type; 
 }
+
+// // Day 16: Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
+
+// For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, but for size 3 x 1 you must do two breaks.
+
+// If input data is invalid you should return 0 (as in no breaks are needed if we do not have any chocolate to split). Input will always be a non-negative integer.
+
+var breakChocolate = function(n, m) {
+  return (n*m === 0) ? 0 : n * m - 1;
+ };
