fea05febd02c48130434be0d696581f59b97c681
Aaron Hu <haldenbiscuit@gmail.com>
2022-09-06 17:35:30 -0700

    Added Day 20

--- main.js
+++ main.js
@@ -173,3 +173,17 @@ function stantonMeasure(arr) {
   const count = n => arr.filter(x => x === n).length;
   return count(count(1));
 }
+
+// Day 20: We want an array, but not just any old array, an array with contents.
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+// For example, the following code will result in an array containing the numbers 0 to 4:
+// arr(5) // => [0,1,2,3,4]
+// Note: The parameter is optional. So you have to give it a default value.
+
+function arr(n){
+  var newArr = [];
+  for(var i = 0; i < n; i++){
+    newArr.push(i);
+  }
+  return newArr;
+}
