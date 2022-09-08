0e3871f7bd3abedd12dbf1cecbd47be7ee84a926
Aaron Hu <haldenbiscuit@gmail.com>
2022-09-07 23:28:50 -0700

    Added Day 21

--- main.js
+++ main.js
@@ -187,3 +187,11 @@ function arr(n){
   }
   return newArr;
 }
+
+//Day  21: Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+// If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+// Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java, Scala, and Julia), "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell, and Lua) or "¥" (Rust).
+
+function bonusTime(salary, bonus) {
+  return bonus ? `£${10 * salary}` : `£${salary}`;
+}
