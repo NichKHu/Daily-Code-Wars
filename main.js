89effe2f95eaf3ae575079b0184eec1a4a4f0cc8
Aaron Hu <haldenbiscuit@gmail.com>
2022-09-10 23:43:44 -0700

    Added Day 24

--- main.js
+++ main.js
@@ -237,3 +237,17 @@ function simpleMultiplication(num) {
     return num * 9
   }
 }
+
+// Day 24: While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
+// checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
+// The function receives one parameter health which will always be a whole number between -10 and 10.
+
+function checkAlive(health) {
+  if(health > 0) {
+    return true
+  } else if(health < 0) {
+    return false
+  } else {
+    return false
+  }
+}
