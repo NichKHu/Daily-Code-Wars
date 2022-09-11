0b23a3970a33acfeb3757f5f3454c2ae8e8844c4
Aaron Hu <haldenbiscuit@gmail.com>
2022-09-10 23:36:32 -0700

    Added Day 23

--- main.js
+++ main.js
@@ -227,3 +227,13 @@ function rentalCarCost(days) {
 function arrayPlusArray(arr1, arr2) {
   return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
 }
+
+//Day 23: This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(num) {
+  if(num % 2 === 0 ) {
+    return num * 8
+  } else {
+    return num * 9
+  }
+}
