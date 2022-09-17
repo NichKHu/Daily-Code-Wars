6f91608e00055ae3dab848e187875fed0aaa0440
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-09-16 23:25:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -289,4 +289,10 @@ function rowSumOddNumbers(n) {
   return Math.pow(n, 3);
 }
 
-//Day 27: 
+//Day 27: Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a,b,c)
+{
+   return a + b > c && a + c > b && c + b > a;
+}
