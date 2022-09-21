38f7f0e5351a4c80153a8b79b684df61dba80426
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-09-20 23:42:23 -0700

    Added Day 33

--- main.js
+++ main.js
@@ -341,3 +341,14 @@ function findAverage(array) {
   let solution = (sumOfArray/array.length);
   return solution;
 }
+
+//Day 33: Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// patrick feeney => P.F
+
+function abbrevName(name){
+  var nameArray = name.split(" ");
+  return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+}
