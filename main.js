78e4dc38cedab04f0ca102527c48c5cd1cd69175
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-09-21 23:23:59 -0700

    Added Day 34

--- main.js
+++ main.js
@@ -352,3 +352,16 @@ function abbrevName(name){
   var nameArray = name.split(" ");
   return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
 }
+
+//Day 34: YouTube had a like and a dislike button, which allowed users to express their opinions about particular content. It was set up in such a way that you cannot like and dislike a video at the same time. There are two other interesting rules to be noted about the interface: Pressing a button, which is already active, will undo your press. If you press the like button after pressing the dislike button, the like button overwrites the previous "Dislike" state. The same is true for the other way round.
+// Task
+// Create a function that takes in a list of button inputs and returns the final state.
+// Examples
+// likeOrDislike([Dislike]) => Dislike
+// likeOrDislike([Like,Like]) => Nothing
+// likeOrDislike([Dislike,Like]) => Like
+// likeOrDislike([Like,Dislike,Dislike]) => Nothing
+// Notes
+// If no button is currently active, return Nothing.
+// If the list is empty, return Nothing.
+
