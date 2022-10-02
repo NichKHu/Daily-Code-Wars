5e1def738d982530aea5dc4395dd6adb03c11f3a
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-10-01 23:35:41 -0700

    Added Day 36

--- main.js
+++ main.js
@@ -410,3 +410,12 @@ function arithmetic(a, b, operator){
       return a / b;
   }
 }
+
+//Day 36: Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+function squareArea(A){
+  var circum = 4 * A;
+  var radius = circum / (2 * Math.PI);
+  var area = Math.pow(radius, 2);
+  return Math.round(area*100)/100
+}
