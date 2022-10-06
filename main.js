cf6af6721990369ddf1e14e0f8f0b187d59df837
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-10-05 23:43:07 -0700

    Added Day 40

--- main.js
+++ main.js
@@ -438,3 +438,17 @@ function evaporator(content, evap_per_day, threshold){
   }
     return days;
   }
+
+//Day 40: Task: Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+//In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained 
+
+function expressionMatter(a, b, c) {
+  return Math.max(
+    a + b + c,
+    a * b * c,
+    a * (b + c),
+    (a + b) * c,
+    a + b * c,
+    a * b + c,
+  );
+}
