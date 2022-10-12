bd0a1a6cbff59c9c5a3c77d690f12402b798ce36
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-10-12 16:54:32 -0700

    Added Day 43

--- main.js
+++ main.js
@@ -504,3 +504,19 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
     return firstAttacker;
   }
 }
+
+//Day 43: You love coffee and want to know what beans you can afford to buy it.
+
+// The first argument to your search function will be a number which represents your budget.
+
+// The second argument will be an array of coffee bean prices.
+
+// Your 'search' function should return the stores that sell coffee within your budget.
+
+// The search function should return a string of prices for the coffees beans you can afford. The prices in this string are to be sorted in ascending order.
+
+let search = (budget, prices) => prices
+    .filter(p => p <= budget)
+    .sort((a, b) => a - b)
+    .join(',')
+
