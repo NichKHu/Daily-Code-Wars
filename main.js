7d3edc0acec2dd212d49ccfc63ac3e5443ec2698
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-10-24 21:18:24 -1000

    Added Day 48

--- main.js
+++ main.js
@@ -591,3 +591,14 @@ function shadesOfGrey(n) {
   
   return shades
 }
+
+//Day 48: Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+(the dedicated builtin(s) functionalities are deactivated)
+
+reverse = function(array) {
+  var newArr = [];
+  for (var i = array.length-1; i>=0; i--){
+    newArr.push(array[i]);
+  }
+  return newArr;
+  }
