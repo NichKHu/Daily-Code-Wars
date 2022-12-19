dc8200f5c04e6edf7507e477ab6eca7e7582ebfc
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-12-18 22:01:22 -1000

    Added Day 54

--- main.js
+++ main.js
@@ -732,3 +732,15 @@ If b = 0,return empty list.
 
 const solution = (n, b) =>
   Array.from({length: 2**n},(_,index) => index).filter(element => element & b)
+
+
+//Day 54: The Collatz conjecture (also known as 3n+1 conjecture) is a conjecture that applying the following algorithm to any number we will always eventually reach one:
+
+[This is writen in pseudocode]
+if(number is even) number = number / 2
+if(number is odd) number = 3*number + 1
+#Task
+
+Your task is to make a function hotpo that takes a positive n as input and returns the number of times you need to perform this algorithm to get n = 1.
+
+const hotpo = n => n === 1 ? 0 : 1 + hotpo(n % 2 ? (n * 3 + 1) : n / 2);
