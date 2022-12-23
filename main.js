ff206a0515dccd79e7486cad1fd67945e26bb7bf
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-12-22 19:57:42 -1000

    Added Day 58

--- main.js
+++ main.js
@@ -790,3 +790,26 @@ function min(a, b){
   if (isNaN(a) || isNaN(b)) { return NaN; }
   return (a < b) ? a : b;
 }
+
+//Day 58: Prince Arthas needs your help! Mal'ganis has spread an infection amongst the Stratholme citizens, and we must help Arthas prevent this infection from spreading to other parts of the Kingdom.
+
+You will receive a string s as input: Each "word" represents a house, and each letter represents a citizen. All infected citizens are represented as "i" or "I" in s. You must eradicate them, and their neighbors. If an infected citizen appears after or before a space, you should not delete the space, but keep in mind that the distance from "house" to "house" (word to word) has to have only one whitespace.
+
+EXAMPLES:
+
+"STRING"                       -> "STG"
+"1i2 33 i4i5 i555ii5"          -> "33 5"
+"It is a bit chilly"           -> "a cly"
+"Pineapple pizza is delicious" -> "eapple za deus"
+"It is not there"              -> "not there"
+
+function purifyTheStratholme(s) {
+  let result = [];
+  for (let i of s.split(' ')) {
+    if (i.replace(/.?i+.?/gi,'') === '') {
+      continue
+    }
+    result.push(i.replace(/.?i+.?/gi,''));
+  }
+  return result.join(' ');
+}
