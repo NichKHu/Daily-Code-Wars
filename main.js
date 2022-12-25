f4ce5fc757a61b3822bd6696b976c0b4135ffab3
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-12-24 20:02:56 -1000

    Added Day 60

--- main.js
+++ main.js
@@ -841,3 +841,23 @@ var comparator = function(a,b) {
   const pos = ['On', '12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', 'a'];
   return pos.indexOf(a.split(' ')[0]) - pos.indexOf(b.split(' ')[0]);
 }
+
+//Day 60: Take a list of n numbers a1, a2, a3, ..., aN to start with.
+
+Arithmetic mean (or average) is the sum of these numbers divided by n.
+
+Geometric mean (or average) is the product of these numbers taken to the nth root.
+
+Example
+List of numbers: 1, 3, 9, 27, 81
+
+n = 5
+Arithmetic mean = (1 + 3 + 9 + 27 + 81) / 5 = 121 / 5 = 24.2
+Geometric mean = (1 * 3 * 9 * 27 * 81) ^ (1/5) = 59049 ^ (1/5) = 9
+Task
+You will be given a list of numbers and their arithmetic mean. However, the list is missing one number. Using this information, you must figure out and return the geometric mean of the FULL LIST, including the number that's missing.
+
+const geo_mean = (a, n) => {
+  const m = n * (a.length + 1) - a.reduce((s, n) => s + n, 0);
+  return Math.pow(m * a.reduce((p, n) => p * n, 1), 1 / (a.length + 1));
+};
