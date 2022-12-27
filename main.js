e465da3f91732996a0f4dae816cd0b7c34e81ef6
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-12-26 22:21:18 -1000

    Added Day 61

--- main.js
+++ main.js
@@ -861,3 +861,12 @@ const geo_mean = (a, n) => {
   const m = n * (a.length + 1) - a.reduce((s, n) => s + n, 0);
   return Math.pow(m * a.reduce((p, n) => p * n, 1), 1 / (a.length + 1));
 };
+
+//Day 61: Grinch wants to spoil Christmas for JavaScript programmers. For that, he devised a hellish plan: pollute their Date prototype so that Christmas never happens. He wants to change method getDate of Date so that it returns 26 for Date objects that represent 25th of December but works correctly for all other days of the year.
+
+However, Grinch does not know how to do that. Can you help him?
+
+Date.prototype.getDate = function getGrinchDate() {
+  let [day, month, date] = this.toString().split(" ");
+  return month == 'Dec' && date == 25 ? 26 : +date;
+};
