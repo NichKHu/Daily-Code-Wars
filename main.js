3a4fabf9ffd67e67c0722d5c1d3ec705303ba462
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-12-28 20:21:28 -1000

    Added Day 63

--- main.js
+++ main.js
@@ -894,3 +894,46 @@ function adjust(coin, price){
   while(price%coin) price++;
   return price
 }
+
+//Day 63: Scheme Reference
+
+// Your challenge: create a function that interprets simple scheme-like math commands and returns the value resulting from the operation. We will only be working with the four main math operators (+ - * /). The main difference between these commands and actual scheme commands is that scheme commands only accept two arguments while these commands accept infinitely many arguments so as to avoid having to nest functions with like operators.
+
+// Input: string, such as '(+ 4 3)'
+// Output: number
+// Constraints: 0-15 arguments, numbers from -15 to 15 (when dividing, those will never be 0)
+// Edge Cases: don't forget that no arguments results in either 0 or 1 for +/- and * respectively
+// Syntax consists of an open parenthesis, a math operator, a space, space-separated arguments, and a close parenthesis. Example: (+ 3 4). All inputs will be valid.
+
+// If no arguments are provided, return the following (You won't get '(/)'):
+
+// '(+)'         // 0
+// '(-)'         // 0
+// '(*)'         // 1
+// Examples of valid commands:
+
+// '(+ 3 4)'     // 7
+// '(- 3 4)'     // -1
+// '(* 3 4)'     // 12
+// '(/ 3 4)'     // 0.75
+// '(+ 4 4 -4)'  // 4
+
+function scheme(cmd) {
+  const formatted = cmd.replace(')', '').replace('(', '');
+  const [operation, ...rest] = formatted.split(' ');
+
+  switch(operation) {
+      case '+': {
+        return rest.reduce((acc, el) => acc = acc + Number(el), 0);
+      }
+      case '*': {
+        return rest.reduce((acc, el) => acc = acc * Number(el), 1);
+      }
+      case '/': {
+        return rest.reduce((acc, el, index) => acc = Number(index === 0 ? el : acc / el),  1);
+      }
+      default: {
+        return rest.reduce((acc, el, index) => acc = Number(index === 0 ? el : acc - el), 0);
+      }
+  }
+}
