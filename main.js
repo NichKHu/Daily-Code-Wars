f6e61c550ce1b43e26458bd9b63b53312efb52b9
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-12-30 22:28:46 -1200

    Added Day 65

--- main.js
+++ main.js
@@ -949,3 +949,14 @@ function scheme(cmd) {
 function solution(str, ending){
   return str.endsWith(ending);
 }
+
+
+//Day 65: Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+// flatten([1,2,3]) // => [1,2,3]
+// flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+// flatten([[[1,2,3]]]) // => [[1,2,3]]
+
+var flatten = function (lol){
+  return [].concat.apply([],lol);
+}
