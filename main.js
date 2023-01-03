7419b2d2dffe40142ac8c72a3255a9d1affd889a
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2023-01-02 19:39:08 -1200

    Added Day 66

--- main.js
+++ main.js
@@ -960,3 +960,12 @@ function solution(str, ending){
 var flatten = function (lol){
   return [].concat.apply([],lol);
 }
+
+/Day 66: Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.
+
+const invert = array => array.map(num => -num);
