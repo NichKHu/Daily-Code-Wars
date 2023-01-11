e58af1ace9b9c0b6f0154cf4c43f8cbbd5d1cb3f
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-01-10 20:03:03 -1200

    Added Day 69

--- main.js
+++ main.js
@@ -1024,4 +1024,63 @@ function findLowestInt(k) {
   const x = k + 1;
   const string = n => n.toString().split``.sort().join``;
   while (1) if (string(n * k) === string(n++ * x)) return --n;
+
+}
+
+//Day 69: #Adding values of arrays in a shifted way
+
+You have to write a method, that gets two parameter:
+
+1. An array of arrays with int-numbers
+2. The shifting value
+#The method should add the values of the arrays to one new array.
+
+The arrays in the array will all have the same size and this size will always be greater than 0.
+The shifting value is always a value from 0 up to the size of the arrays.
+There are always arrays in the array, so you do not need to check for null or empty.
+
+#1. Example:
+
+[[1,2,3,4,5,6], [7,7,7,7,7,-7]], 0
+
+1,2,3,4,5,6
+7,7,7,7,7,-7
+
+--> [8,9,10,11,12,-1]
+#2. Example
+
+[[1,2,3,4,5,6], [7,7,7,7,7,7]], 3
+
+1,2,3,4,5,6
+      7,7,7,7,7,7
+
+--> [1,2,3,11,12,13,7,7,7]
+#3. Example
+
+[[1,2,3,4,5,6], [7,7,7,-7,7,7], [1,1,1,1,1,1]], 3
+
+
+1,2,3,4,5,6
+      7,7,7,-7,7,7
+            1,1,1,1,1,1
+
+--> [1,2,3,11,12,13,-6,8,8,1,1,1]
+
+function addingShifted (arrayOfArrays, shift) {    
+  var result = [];
+  var endArrayLength = arrayOfArrays[0].length + shift * (arrayOfArrays.length - 1);
+  for(var i=0;i<endArrayLength;i++)
+  {
+    result.push(0);
+  }
+      
+  for(var i=0;i<arrayOfArrays.length;i++)
+  {
+    for(var j=0;j<arrayOfArrays[i].length;j++)          
+    {
+      result[j + shift * i] += arrayOfArrays[i][j];
+    }          
+  }
+      
+  return result;
 }
