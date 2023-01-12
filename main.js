82781f70f741be6b69bd6162472529428ff99861
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-01-11 21:44:16 -1200

    Added Day 70

--- main.js
+++ main.js
@@ -1084,3 +1084,22 @@ function addingShifted (arrayOfArrays, shift) {
       
   return result;
 }
+
+Day 70: Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+Your function should only return a number, not the explanation about how you get that number.
+
+const GetSum = (a, b) => {
+  let min = Math.min(a, b),
+      max = Math.max(a, b);
+  return (max - min + 1) * (min + max) / 2;
+}
