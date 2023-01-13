78b1564bd7d13d77d812eaded395f553e73d3520
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-01-12 22:12:44 -1200

    Added Day 71

--- main.js
+++ main.js
@@ -1103,3 +1103,10 @@ const GetSum = (a, b) => {
       max = Math.max(a, b);
   return (max - min + 1) * (min + max) / 2;
 }
+
+Day 71: I have four positive integers, A, B, C and D, where A < B < C < D. The input is a list of the integers A, B, C, D, AxB, BxC, CxD, DxA in some order. Your task is to return the value of D.
+
+function alphabet(ns) {
+  let sorted = ns.sort((a,b) => a - b)
+  return sorted[7] / sorted[sorted[0] * sorted[1] == sorted[2] ? 3 : 2]
+}
