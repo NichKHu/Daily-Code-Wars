accc9777e4a6ba9f9f82a4c3f7907f6436efdf23
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-01-15 21:47:40 -1200

    Added Day 73

--- main.js
+++ main.js
@@ -1154,3 +1154,22 @@ var twoSum = function(nums, target) {
     }
 };
 
+//Day 73: 
+Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+Notes:
+Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given. See the examples below for further details.
+
+If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+const initialCap = (str) => str[0].toUpperCase();
+const isValidName = (name) => /^[a-z]/i.test(name);
+const aliasGen = (fName, lName) => {
+  return (isValidName(fName) && isValidName(lName))
+    ? `${ firstName[initialCap(fName)] } ${ surname[initialCap(lName)] }`
+    : 'Your name must start with a letter from A - Z.';
+}
