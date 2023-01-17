42f8127ee68a1802ea58bd9f4cbb101be4cb86da
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-01-16 20:49:07 -1200

    Added Day 74

--- main.js
+++ main.js
@@ -1173,3 +1173,13 @@ const aliasGen = (fName, lName) => {
     ? `${ firstName[initialCap(fName)] } ${ surname[initialCap(lName)] }`
     : 'Your name must start with a letter from A - Z.';
 }
+
+Day 74: You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+function otherAngle(a, b) {
+  return 180-(a+b);
+}
