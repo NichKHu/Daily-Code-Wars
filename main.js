8a169c3c090cb938bd487d410484356ac4a3bf33
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-01-17 20:24:38 -1200

    Added Day 75

--- main.js
+++ main.js
@@ -1183,3 +1183,10 @@ Note: only positive integers will be tested.
 function otherAngle(a, b) {
   return 180-(a+b);
 }
+
+Day 75: I have four positive integers, A, B, C and D, where A < B < C < D. The input is a list of the integers A, B, C, D, AxB, BxC, CxD, DxA in some order. Your task is to return the value of D.
+
+function alphabet(ns) {
+  let sorted = ns.sort((a,b) => a - b)
+  return sorted[7] / sorted[sorted[0] * sorted[1] == sorted[2] ? 3 : 2]
+}
