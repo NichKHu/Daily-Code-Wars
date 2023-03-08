a51ef99fd4625b8cd3871c800ad8cc4480143cd3
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-03-07 20:09:00 -1200

    Added Day 76

--- main.js
+++ main.js
@@ -1192,3 +1192,18 @@ function alphabet(ns) {
 }
 
 //Reviewed changes
+
+Day 76: 
+
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+import string
+
+def is_pangram(s):
+    s = s.lower()
+    for char in 'abcdefghijklmnopqrstuvwxyz':
+        if char not in s:
+            return False
+    return True
